ec4f075920c2925072d069e398d61d155a6df407
EthanBosen <ethbosen@gmail.com>
2026-04-17 15:21:26 -0400

    Fix duplicate route + add correct /services route for Heroku

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-const path = require('path');  
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'Public')));  
+app.use(express.static(path.join(__dirname, 'Public')));
 
-// Root route
+// Routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'Public', 'home', 'home.html'));
 });
@@ -19,11 +19,11 @@ app.get('/catalog', (req, res) => {
   res.sendFile(path.join(__dirname, 'Public', 'catalog', 'catalog.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/services', (req, res) => {                    
   res.sendFile(path.join(__dirname, 'Public', 'services', 'services.html'));
 });
 
-
+// TODO: Add contact form route later if needed
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
